c811d3566e90a7f4af3347af4e8814c3621b0b8a
Nathan Cashman <nathan.t.cashman@gmail.com>
2022-06-29 18:39:53 +0100

    second file support added and combined data now outputs as expected

--- device_report.py
+++ device_report.py
@@ -2,16 +2,16 @@ import csv
 import sys
 
 # Check that two files have been provided, otherwise print guide on usage and exit
-if len(sys.argv) < 2:
+if len(sys.argv) < 3:
     print("usage: python device_report.py [kibana_file] [oss_file]")
     print("example: python device_report.py /home/ncashman/kibana_404_28Jun.csv /home/ncashman/oss_404_28Jun.csv")
     sys.exit(0)
 
 # set file variables from the OSS and Kibana reports for manipulation
 kibana_file = sys.argv[1]
-
-# oss_file = sys.argv[2]
+oss_file = sys.argv[2]
 kibana_mac = []
+combined_data = []
 
 # pull the lineport from the kibana file as we want to preserve this to replace the one provided in OSS' report
 with open(kibana_file) as csv_file:
@@ -23,12 +23,25 @@ with open(kibana_file) as csv_file:
         else:
             kibana_mac.append(row[6])
 
-with open('/Users/nathancashman/Documents/testing.csv', mode='w', newline='') as outfile:
-    outfile_writer = csv.writer(outfile)
-    for item in kibana_mac:
-            kibana_mac.append(row[0])
+# pull required data from the OSS file and combine this with the lineport from kibana that we gathered earlier
+with open(oss_file) as oss_file_csv:
+    odata = csv.reader(oss_file_csv, delimiter=',')
+    lineCount = 0
+    i = 0
+    for row in odata:
+        if lineCount == 0:
+            combined_data.append(row)
+        else:
+            combined_data.append([row[0], row[1], row[2], row[3], row[4], row[5], kibana_mac[i]])
+            i += 1
+
+# write to the new file
+# with open('/Users/nathancashman/Documents/testing.csv', mode='w', newline='') as outfile:
+#     outfile_writer = csv.writer(outfile)
+#     for item in kibana_mac:
+#         outfile_writer.writerow([item])
 
 with open('/Users/nathancashman/Documents/testing.csv', mode='w', newline='') as outfile:
     outfile_writer = csv.writer(outfile)
-    for item in kibana_file:
+    for item in combined_data:
         outfile_writer.writerow([item])
